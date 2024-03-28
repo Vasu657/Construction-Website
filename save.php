e39e6c4bc91fdcca1270d004cfd676670d9d718d
Vasu <120580133+Vasu657@users.noreply.github.com>
2024-03-28 10:06:11 +0530

    added html and php code

---
+++ save.php
@@ -0,0 +1,13 @@
+
+<?php
+
+$fistname = $_POST["fname"];
+$lastname = $_POST["lname"];
+$email = $_POST["email"];
+$mobile = $_POST["mobile"];
+$conn = mysqli_connect("localhost", "root", "", "mallikarjun") or die("connection failed");
+$sql = "INSERT INTO contact_page(First_name, Last_name, Email, Mobile) VALUES ('{$fistname}','{$lastname}','{$email}','{$mobile}' )";
+$result = mysqli_query($conn, $sql) or die("Query Failed!");
+header("location: http://localhost/Mallikarjun/#contact");
+mysqli_close($conn);
+?>
