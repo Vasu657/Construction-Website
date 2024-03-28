65a9476f858392a8df8fea12ecf88021481f5edc
Vasu <120580133+Vasu657@users.noreply.github.com>
2024-03-28 10:13:20 +0530

    Added Flolders

---
+++ js/script.js
@@ -0,0 +1,85 @@
+// ---------------Home-Slider Starts---------------//
+    
+   var swiper = new Swiper('.home-slider', {
+   
+      loop: true,
+   autoplay: {
+     delay: 10000, // 1 minute delay between slides
+     disableOnInteraction: false,
+   },
+   slidesPerView: 1, // Number of slides per view
+   spaceBetween: 10, // Space between slides in pixels
+}
+);
+
+// ---------------Home-Slider Ends---------------//
+
+
+
+
+// ---------------Testimonials-slider Starts---------------//
+
+var swiper = new Swiper('.Testimonials-slider', {
+   loop: true,
+   autoplay: {
+     delay: 10000, // 1 minute delay between slides
+     disableOnInteraction: false,
+   },
+   slidesPerView: 1, // Number of slides per view
+   spaceBetween: 10, // Space between slides in pixels
+  
+   // Responsive breakpoints
+   breakpoints: {
+    // When window width is <= 768px (mobile)
+    768: {
+       slidesPerView: 3, // Display only one slide
+    },
+    // Add more breakpoints if needed
+ }
+ });
+
+ // ---------------Testimonials-Slider Ends---------------//
+
+
+
+
+
+// ---------------Blogs-Slider---------------//
+
+ var swiper = new Swiper('.blogs-slider', {
+   loop: true,
+   autoplay: {
+     delay: 10000, // 1 minute delay between slides
+     disableOnInteraction: false,
+   },
+   slidesPerView: 1, // Number of slides per view
+   spaceBetween: 10, // Space between slides in pixels
+});
+
+// ---------------Blogs-Slider Ends---------------//
+
+ 
+ 
+
+
+
+// ---------------Logo-Slider Starts---------------//
+
+ var swiper = new Swiper('.logo-slider', {
+   loop: true,
+   autoplay: {
+     delay: 1700, // 2 seconds delay between slides
+     disableOnInteraction: false,
+   },
+   slidesPerView: 4, // Number of slides per view
+   spaceBetween: 10, // Space between slides in pixels
+
+ });
+
+// ---------------Logo-Slider Ends---------------//
+
+
+
+
+
+
